7bbda03ed43dcf4a15788bfdc2a88fe9041e1a68
michaelmaben <jeevanmaben@yahoo.com>
2020-09-23 17:59:54 -0400

    move utility code to helper

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,9 @@
 class GroupsController < ApplicationController
-
+  include GroupsHelper
   def create_groups
     employees = Employee.random.to_a
     group_size = return_group_size(employees)
     @groups = []
-    unassigned_employees = employees.size%group_size
     group_number = 0
     while employees.size > 0
       pop_size = group_size <= employees.size ? group_size : employees.size
@@ -14,14 +13,4 @@ class GroupsController < ApplicationController
     render json: @groups
   end
 
-  private
-    def return_group_size(employees)
-      size = employees.size
-      return 3 if size%3 ==0
-      return 4 if size%4 ==0
-      return 5 if size%5 ==0
-      min =  size >= 3 ? 3 : size
-      max =  size >= 5 ? 5 : size
-      return rand(min..max)
-    end
 end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,11 @@
 module GroupsHelper
+  def return_group_size(employees)
+    size = employees.size
+    return 3 if size%3 ==0
+    return 4 if size%4 ==0
+    return 5 if size%5 ==0
+    min =  size >= 3 ? 3 : size
+    max =  size >= 5 ? 5 : size
+    return rand(min..max)
+  end
 end
